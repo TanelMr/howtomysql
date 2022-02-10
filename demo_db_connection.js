061334e0d774e094b43b0b39c82fbaa2ccc43141
Tanel Märjama <tanel.marjama@khk.ee>
2022-02-10 14:35:22 +0200

    Inserted multiple data into table

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -10,9 +10,15 @@ var con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "INSERT INTO customers (name, address) VALUES ('Tanel', 'Kopli 1')";
-    con.query(sql, function (err, result) {
+    var sql = "INSERT INTO customers (name, address) VALUES ?";
+    var values = [
+        ['Siim', 'Peetri 3'],
+        ['Kevin', 'Salme 4'],
+        ['Sander', 'Nõgiaru 3'],
+        ['Karl', 'Näituse 5']
+    ];
+    con.query(sql, [values], function (err, result) {
         if (err) throw err;
-        console.log("1 record inserted");
+        console.log("Number of records inserted:" + result.affectedRows);
     });
 });

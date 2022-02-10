0368ecc6d3d72ee3c1bbad5699065afad0c87f0c
Tanel Märjama <tanel.marjama@khk.ee>
2022-02-10 14:47:23 +0200

    Selected data using where clause

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -9,8 +9,8 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT name FROM customers", function (err, result, fields) {
+    con.query("SELECT * FROM customers WHERE address = 'Kopli 1'", function (err, result, fields) {
         if (err) throw err;
-        console.log(fields);
+        console.log(result);
     });
 });

043e6a220a71d203e6e2c9b2d762cd5076db8403
Tanel Märjama <tanel.marjama@khk.ee>
2022-02-10 14:54:39 +0200

    Selected data from table and used order by function

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -9,8 +9,7 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    var adr = 'Salme 4'
-    var sql = "SELECT * FROM customers WHERE address = " + mysql.escape(adr)
+    var sql = "SELECT * FROM customers order by Name";
     con.query(sql, function (err, result, fields) {
         if (err) throw err;
         console.log(result);
